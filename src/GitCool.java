1164ab7fb1eb9b5322e57267fc0a734d51c13b8a
Elza Safiullina <e_safa@mail.ru>
2021-08-01 13:40:31 -0500

    new changes in new file

--- src/GitCool.java
+++ src/GitCool.java
@@ -1,2 +1,7 @@
 public class GitCool {
+    public static void main(String[] args) {
+        System.out.println("Im learning GIT");
+        System.out.println("It's easier than I though");
+        System.out.println("I need to keep practicing");
+    }
 }
